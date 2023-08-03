1d6c605a51f9cba73f00b6dd9b0e3450d529e39b
sarahoco014 <soconnor2@outlook.com>
2023-08-03 21:36:30 +0100

    adds extension

--- task_01/src/main/java/com/bnta/task_01/controllers/GreetingController.java
+++ task_01/src/main/java/com/bnta/task_01/controllers/GreetingController.java
@@ -1,6 +1,7 @@
 package com.bnta.task_01.controllers;
 
 
+import com.bnta.task_01.models.Celebration;
 import com.bnta.task_01.models.Greeting;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,7 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class GreetingController {
 
     @GetMapping("/greeting")
-    public Greeting greeting() {
-        return new Greeting("Bob", "Good morning!");
+    public Greeting greeting(@RequestParam(defaultValue = "Welcome!") String timeOfDay) {
+        return new Greeting("Bob", timeOfDay);
+    }
+
+    @GetMapping("/greeting/christmas")
+    public Celebration celebration() {
+        return new Celebration("Merry Christmas!");
     }
 }
---
+++ task_01/src/main/java/com/bnta/task_01/models/Celebration.java
@@ -0,0 +1,21 @@
+package com.bnta.task_01.models;
+
+public class Celebration {
+
+    private String message;
+
+    public Celebration(String message) {
+        this.message = message;
+    }
+
+    public Celebration() {
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
